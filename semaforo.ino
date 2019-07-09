d64e57ab0c5d72e0a29820c3967e083f29c6959e
Davi Costa <50250514+DMCDavi@users.noreply.github.com>
2019-07-09 15:40:14 -0300

    Create semaforo.ino

---
+++ semaforo.ino
@@ -0,0 +1,31 @@
+void setup(){
+  
+  pinMode(13, OUTPUT);
+  
+  pinMode(12, OUTPUT);
+  
+  pinMode(8, OUTPUT);
+  
+}
+
+void loop(){
+  
+  digitalWrite(13, HIGH);
+  
+  delay(6000); 
+  
+  digitalWrite(13, LOW);
+  
+  digitalWrite(12, HIGH);
+  
+  delay(2000); 
+  
+  digitalWrite(12, LOW);
+  
+  digitalWrite(8, HIGH);
+  
+  delay(3000); 
+  
+  digitalWrite(8, LOW);
+
+}
