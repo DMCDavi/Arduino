6323220f0a4fad86772a1add5859c9dcca728c71
Davi Costa <davi.machado.costa@hotmail.com>
2020-10-01 11:50:42 -0300

    Colocando alguns códigos em pastas

--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -2,30 +2,17 @@ void setup(){
   
   pinMode(13, OUTPUT);
   
-  pinMode(12, OUTPUT);
-  
-  pinMode(8, OUTPUT);
-  
 }
 
 void loop(){
   
   digitalWrite(13, HIGH);
   
-  delay(6000); 
+  delay(2000); 
   
   digitalWrite(13, LOW);
   
-  digitalWrite(12, HIGH);
-  
   delay(2000); 
   
-  digitalWrite(12, LOW);
-  
-  digitalWrite(8, HIGH);
-  
-  delay(3000); 
-  
-  digitalWrite(8, LOW);
 
 }
